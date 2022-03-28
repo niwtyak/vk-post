2d625555d678b08df27248ef0c6062d0d18100ce
niwtyak <alex.pakschin@gmail.com>
2022-03-28 18:07:37 +0300

    Add and update functions added

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,4 +1,22 @@
 object WallService {
     private var posts = emptyArray<Post>()
+    private var globalId = 1
 
+    fun add(post: Post): Post {
+        posts += post.copy(id = globalId++)
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, oldPost) in posts.withIndex()) {
+            if (oldPost.id == post.id) {
+                posts[index] = post.copy(
+                    ownerId = oldPost.ownerId,
+                    date = oldPost.date
+                )
+                return true
+            }
+        }
+        return false
+    }
 }

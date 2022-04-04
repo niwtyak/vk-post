f898d07c194b88c98f044063569c01bcd33904b1
niwtyak <alex.pakschin@gmail.com>
2022-04-04 15:44:04 +0300

    Post updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,35 @@
-fun main(){
+import additionalClasses.*
 
+fun main(){
+    val newPost =
+        Post(
+            0,
+            1,
+            1,
+            1,
+            290322,
+            "new post",
+            0,
+            0,
+            false,
+            Comment(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true),
+            Copyright(1, "link", "name", "type"),
+            Likes(3, userLikes = true, canLike = true, canPublish = true),
+            Reposts(0, false),
+            Views(10),
+            "post type",
+            PostSource("vk", null, "likes", "url"),
+            Geo("sity", "123.123.213", Place()),
+            1,
+            null,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = false,
+            markedAsAds = false,
+            isFavorite = false,
+            Donut(false, 0, Placeholder(), true, "edit mode"),
+            0
+        )
+    println(newPost)
 }
